574fd7f22de598883aa9c95f0f0a2e6f4c9c4414
Isheta <102668226+Isheta20@users.noreply.github.com>
2024-04-27 12:00:30 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,16 @@
 print('Isheta Aggarwal')
+def maximum(a, b, c): 
+    if (a >= b) and (a >= c): 
+        largest = a 
+    elif (b >= a) and (b >= c): 
+        largest = b 
+    else: 
+        largest = c 
+    return largest 
+
+# User input
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+
+print("The largest number is:", maximum(a, b, c))
